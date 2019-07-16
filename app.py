ddd120736b48021a348998b29dc856d4be2c11e6
Aditya <adityadpandey@gmail.com>
2019-07-16 22:33:08 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -12,38 +12,26 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
 
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
 
 # Configure application
 app = Flask(__name__)
 
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
 # Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Ensure responses aren't cached
-
-
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use postgres database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
 
 @app.route("/")
 @login_required
@@ -150,7 +138,6 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         x = db.execute(text('SELECT * FROM users')).fetchall()
-        print(x)
         return render_template("login.html")
 
 
